06b9eadb9e695c6b9040fe6b0a3ca86ab8b5b7f1
superizer <superizer@demons>
2015-01-20 14:17:10 +0700

    update json output

--- kraken/json_builder.py
+++ kraken/json_builder.py
@@ -6,5 +6,5 @@ class KrakenJsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, DraggableWidget): 
             pos = dict(x=obj.x,y=obj.y)
-            return dict(in_cv=obj.in_cv, out_cv=obj.out_cv, pos=pos, name=obj.name, id=obj.id)
+            return dict(in_cv=obj.in_cv, out_cv=obj.out_cv, pos=pos, name=obj.name, id=obj.id, pars=obj.pars)
         return json.JSONEncoder.default(self, obj)
